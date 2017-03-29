f463382f06e9e02926ebe70452115859443e2674
yohh <yohh@cisco.com>
2017-03-29 17:43:16 -0400

    authentication api test

---
+++ step 1/authentication-api.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://example.com/api/3.3/login"
+
+headers = {
+    'content-type': "application/json",
+    'cache-control': "no-cache",
+    'postman-token': "60b1691f-fcc5-34dc-c385-41cf77d41a13"
+    }
+
+response = requests.request("POST", url, headers=headers)
+
+print(response.text)
